0fc02df0397c92f05ac4b9bcd2c9169eb5c91128
pa964 <p3621630@gmail.com>
2025-03-24 19:36:15 +0530

    added index.js

---
+++ index.js
@@ -0,0 +1,58 @@
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+    });
+}
+
+document.addEventListener("keypress", function (event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+})
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var tom1 = new Audio('/audio/tom-1.mp3');
+            tom1.play();
+            break;
+
+        case "a":
+            var tom2 = new Audio('/audio/tom-2.mp3');
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio('/audio/tom-3.mp3');
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio('/audio/tom-4.mp3');
+            tom4.play();
+            break;
+        case "j":
+            var snare = new Audio('/audio/snare.mp3');
+            snare.play();
+            break;
+        case "k":
+            var crash = new Audio('/audio/crash.mp3');
+            crash.play();
+            break;
+        case "l":
+            var kick = new Audio('/audio/kick-bass.mp3');
+            kick.play();
+            break;
+        default:
+            break;
+    }
+}
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100);
+}
