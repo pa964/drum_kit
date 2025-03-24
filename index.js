e590b185f9944940174c9c19c56adfc97c521704
pa964 <p3621630@gmail.com>
2025-03-24 19:42:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,32 +17,32 @@ document.addEventListener("keypress", function (event) {
 function makeSound(key) {
     switch (key) {
         case "w":
-            var tom1 = new Audio('/audio/tom-1.mp3');
+            var tom1 = new Audio('audio/tom-1.mp3');
             tom1.play();
             break;
 
         case "a":
-            var tom2 = new Audio('/audio/tom-2.mp3');
+            var tom2 = new Audio('audio/tom-2.mp3');
             tom2.play();
             break;
         case "s":
-            var tom3 = new Audio('/audio/tom-3.mp3');
+            var tom3 = new Audio('audio/tom-3.mp3');
             tom3.play();
             break;
         case "d":
-            var tom4 = new Audio('/audio/tom-4.mp3');
+            var tom4 = new Audio('audio/tom-4.mp3');
             tom4.play();
             break;
         case "j":
-            var snare = new Audio('/audio/snare.mp3');
+            var snare = new Audio('audio/snare.mp3');
             snare.play();
             break;
         case "k":
-            var crash = new Audio('/audio/crash.mp3');
+            var crash = new Audio('audio/crash.mp3');
             crash.play();
             break;
         case "l":
-            var kick = new Audio('/audio/kick-bass.mp3');
+            var kick = new Audio('audio/kick-bass.mp3');
             kick.play();
             break;
         default:
